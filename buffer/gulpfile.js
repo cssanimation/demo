313d7866ac33aa14bdda7681b9f4fdb2687cda02
Donovan Hutchinson <donovan@kitmanlabs.com>
2015-05-22 22:54:55 +0100

    Adding files

--- buffer/gulpfile.js
+++ buffer/gulpfile.js
@@ -20,7 +20,7 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function() {
   gulp.src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({style: 'expanded'}).on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('stylesheets'))
     .pipe(rename({suffix: '.min'}))
